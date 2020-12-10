0e2c2a2d894af26979c8e0768c46610e6429c277
nhhoang188 <72209266+nhhoang188@users.noreply.github.com>
2020-12-10 21:09:40 +0700

    add gettime

--- src/program/Main.java
+++ src/program/Main.java
@@ -1,7 +1,31 @@
 package program;
+import controler.ShowSchedules;
+import controler.ShowStandings;
 
-import java.util.List;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 
 public class Main {
-    List<S>
+
+    public static void main(String[] args) {
+            gettime();
+//        ShowSchedules schedules = new ShowSchedules();
+//        schedules.showAll("ListMatch.CSV");
+//        schedules.update("ListMatch.CSV");
+//        schedules.showByTeam("ListMatch.CSV");
+//        schedules.showNextRound("ListMatch.CSV");
+//
+//
+//        ShowStandings standings = new ShowStandings();
+//
+//        standings.showAll("ListTeam.CSV");
+//        standings.update("ListTeam.CSV");
+//        standings.showAll("ListTeam.CSV");
+    }
+    public static void gettime(){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("Today is: " + dtf.format(now));
+    }
 }
